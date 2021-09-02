290667636a7a346d59eeadddb385812485c24ad0
Yarden Morshtein <yardenmorshtein@pingidentity.com>
2021-09-02 19:30:06 +0300

    is working

--- src/test/java/tests/CalcAutoTests.java
+++ src/test/java/tests/CalcAutoTests.java
@@ -2,10 +2,7 @@ package tests;
 
 import base.BaseTests;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by yardenmorshtein on 03/09/2021.
@@ -23,31 +20,60 @@ public class CalcAutoTests extends BaseTests {
     }
 
     @Test(priority = 1)
-    public void checkFor2plus3() {
-        calcActions.clickOnNumButton(2);
+    public void checkFor2plus3() throws InterruptedException {
+        calcActions.clickCalcNumButton(2);
         calcActions.clickOnPlusButton();
-        calcActions.clickOnNumButton(3);
+        calcActions.clickCalcNumButton(3);
         calcActions.clickOnEqualsButton();
-        Assert.assertEquals(calcActions.getCalcResult(),"=5");
-
+        Assert.assertEquals(calcActions.getCalcResult(),"5");
     }
 
     @Test(priority = 2)
-    public void checkFor10minus2() {
-
+    public void checkFor10minus2() throws InterruptedException {
+        calcActions.clickCalcNumButton(1);
+        calcActions.clickCalcNumButton(0);
+        calcActions.clickOnMinusButton();
+        calcActions.clickCalcNumButton(2);
+        calcActions.clickOnEqualsButton();
+        Assert.assertEquals(calcActions.getCalcResult(),"8");
     }
 
     @Test(priority = 3)
-    public void checkFor10minus2AndMore() {
-
+    public void checkFor10minus2AndMore() throws InterruptedException {
+        calcActions.clickOnLeftParenthesesCalcButton();
+        calcActions.clickCalcNumButton(1);
+        calcActions.clickCalcNumButton(0);
+        calcActions.clickOnMinusButton();
+        calcActions.clickCalcNumButton(2);
+        calcActions.clickOnRightParenthesesCalcButton();
+        calcActions.clickOnMultiplyButton();
+        calcActions.clickCalcNumButton(2);
+        calcActions.clickOnEqualsButton();
+        Assert.assertNotEquals(calcActions.getCalcResult(),"20");
     }
 
     @Test(priority = 4)
-    public void checkForSin30() {
+    public void checkForSin30() throws InterruptedException {
+        calcActions.clickOnSinCalcButton();
+        calcActions.clickCalcNumButton(3);
+        calcActions.clickCalcNumButton(0);
+        calcActions.clickOnRightParenthesesCalcButton();
+        calcActions.clickOnEqualsButton();
+        Assert.assertEquals(calcActions.getCalcResult(),"0.5");
+    }
 
+    @Test(priority = 5)
+    public void checkFormulasValidation() throws InterruptedException {
+        calcActions.calcGetAllHistory();
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        calcActions.clickOnClearCalcButton();
     }
 
     @AfterClass(alwaysRun = true)
     public void afterClass() {
+//        driver.close();
     }
 }
